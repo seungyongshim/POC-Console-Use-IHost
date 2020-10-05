34dca128430d25feca787a37d4a321c37578123a
심승용 <seungyong.shim@mirero.co.kr>
2020-10-05 13:31:53 +0900

    샘플 작성

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((h, s) =>
+                {
+                    s.AddHostedService<Worker>();
+                });
+
+        private static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # POC-Console-Use-IHost
+- https://garywoodfine.com/ihost-net-core-console-applications/
+- https://docs.microsoft.com/ko-kr/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-5.0
